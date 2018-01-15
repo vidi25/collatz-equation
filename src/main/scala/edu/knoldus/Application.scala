4e2bd61c898d8dc6b979ceaad61abb223158f028
Vidisha Gupta <vidisha.gupta@knoldus.in>
2018-01-15 22:08:05 +0530

    collatz equation problem corrected

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -7,7 +7,7 @@ object Application extends App {
   val log = Logger.getLogger(this.getClass)
   val app = new Function
   val inputNumber1 = 9
-  val inputNumber2 = 4
+  val inputNumber2 = 7
   val list = app.collatzFunction(inputNumber1, Nil,Nil)
   log.info(s"List is ${app.collatzFunction(inputNumber2,Nil,list)}")
 
